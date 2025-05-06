526ce81577ed21c95d8ed3df1014ce84eed55a99
Akashgit17 <akashs9492@gmail.com>
2025-05-06 17:11:43 +0530

    06

--- delivery/views.py
+++ delivery/views.py
@@ -108,3 +108,8 @@ def delete_restaurant(request, restaurant_id):
 def open_update_menu(request, restaurant_id):
     restaurant = Restaurant.objects.get(id = restaurant_id)
     return render(request, 'delivery/update_menu.html',{"restaurant" : restaurant})
+
+def update_menu(request, restaurant_id):
+    restaurant = Restaurant.objects.get(id = restaurant_id)
+    
+    return HttpResponse("received")
