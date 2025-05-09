f123d55cc47828ce07035ba1e7df8e44c1bec5b3
Akashgit17 <akashs9492@gmail.com>
2025-05-09 19:58:31 +0530

    9th

--- delivery/urls.py
+++ delivery/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
 
     path('view_menu/<int:restaurant_id>/<str:username>', views.view_menu, name='view_menu'), 
     path('add_to_cart/<int:item_id>/<str:username>', views.add_to_cart, name='add_to_cart'), 
+     path('show_cart/<str:username>', views.show_cart, name='show_cart'), 
 ]
