c51ca0852eef1b32dcbd080f3c3ec611c940b522
Akashgit17 <akashs9492@gmail.com>
2025-05-13 20:24:08 +0530

    Razorpay added

--- meal_buddy/settings.py
+++ meal_buddy/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RAZORPAY_KEY_ID = 'rzp_test_LS0ZNUfzOWDjR9'
+RAZORPAY_KEY_SECRET = 'H2PKbLJoo6kbgJpLvSSDzf8u'
